e6fe0065ae17aa70bf0a8b612c3bb0e458910849
Dave Smith <getdavemail@gmail.com>
2020-01-21 13:36:04 +0000

    Add state and UI to represent link being in a resolving/loading state
    This is require to accommodate the new async mode for setting a link. For example when a Page is being created async with the API.

--- packages/block-editor/src/components/link-control/index.js
+++ packages/block-editor/src/components/link-control/index.js
@@ -96,7 +96,8 @@ function LinkControl( {
 	const instanceId = useInstanceId( LinkControl );
 	const [ inputValue, setInputValue ] = useState( ( value && value.url ) || '' );
 	const [ isEditingLink, setIsEditingLink ] = useState( ! value || ! value.url );
-	const isEndingEditWithFocus = useRef( false );
+    const [ isResolvingLink, setIsResolvingLink ] = useState( false );
+
 	const { fetchSearchSuggestions } = useSelect( ( select ) => {
 		const { getSettings } = select( 'core/block-editor' );
 		return {
@@ -257,7 +258,7 @@ function LinkControl( {
 						<LinkControlSearchCreate
 							searchTerm={ inputValue }
 							onClick={ async () => {
-								setIsEditingLink( false );
+								setIsResolvingLink( true );
 								onChange( {
 									title: 'Loading link...',
 									url: 'loading...',
@@ -270,6 +271,7 @@ function LinkControl( {
 										} );
 									}, 5000 );
 								} );
+								setIsResolvingLink( false );
 								setIsEditingLink( false );
 								onChange( _result );
 							} }
@@ -283,10 +285,30 @@ function LinkControl( {
 
 	return (
 		<div
+
 			tabIndex={ -1 }
 			ref={ wrapperNode }
 			className="block-editor-link-control"
 		>
+            { isResolvingLink && (
+            <div
+                className={ classnames( 'block-editor-link-control__search-item', {
+                    'is-current': true,
+                } ) }
+            >
+                <span className="block-editor-link-control__search-item-header">
+                    <span
+                        className="block-editor-link-control__search-item-title"
+                    >
+                        { __( 'Creating Page' ) }
+                    </span>
+                    <span className="block-editor-link-control__search-item-info">
+                        { __( 'Your new Page is being created' ) }.
+                    </span>
+                </span>
+            </div>
+
+
 			{ isEditingLink || ! value ?
 				<LinkControlSearchInput
 					value={ inputValue }
@@ -300,6 +322,9 @@ function LinkControl( {
 					onReset={ resetInput }
 					showInitialSuggestions={ showInitialSuggestions }
 				/> :
+			) }
+
+			{ ( ! isEditingLink && ! isResolvingLink ) && (
 				<Fragment>
 					<p className="screen-reader-text" id={ `current-link-label-${ instanceId }` }>
 						{ __( 'Currently selected' ) }:
@@ -340,6 +365,25 @@ function LinkControl( {
 					/>
 				</Fragment>
 			}
+
+			{ isEditingLink && ! isResolvingLink && (
+				<LinkControlSearchInput
+					value={ inputValue }
+					onChange={ onInputChange }
+					onSelect={ ( suggestion ) => {
+						setIsEditingLink( false );
+						onChange( { ...value, ...suggestion } );
+					} }
+					renderSuggestions={ renderSearchResults }
+					fetchSuggestions={ getSearchHandler }
+					onReset={ resetInput }
+					showInitialSuggestions={ showInitialSuggestions }
+				/>
+			) }
+
+			{ ! isEditingLink && ! isResolvingLink && (
+				<LinkControlSettingsDrawer value={ value } settings={ settings } onChange={ onChange } />
+			) }
 		</div>
 	);
 }
