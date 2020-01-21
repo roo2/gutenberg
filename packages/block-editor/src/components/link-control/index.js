a8dfec7363b547fa062b386f4011e23c0cd51bc1
Marek Hrabe <marekhrabe@me.com>
2020-01-21 14:23:41 +0100

    actually create pages, not posts

--- packages/block-editor/src/components/link-control/index.js
+++ packages/block-editor/src/components/link-control/index.js
@@ -263,7 +263,7 @@ function LinkControl( {
 									url: 'loading...',
 								} );
 								const newPage = await apiFetch( {
-									path: `/wp/v2/posts`,
+									path: `/wp/v2/pages`,
 									data: {
 										title: inputValue,
 										content: '',
