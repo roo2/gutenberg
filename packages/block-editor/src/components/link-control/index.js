f899a4dedef0bafd6ce4650a1341171dc2e63afe
Marek Hrabe <marekhrabe@me.com>
2020-01-21 14:19:14 +0100

    add basic API integration (inline in component)

--- packages/block-editor/src/components/link-control/index.js
+++ packages/block-editor/src/components/link-control/index.js
@@ -90,7 +90,6 @@ function LinkControl( {
 	settings,
 	onChange = noop,
 	showInitialSuggestions,
-	setTimeout,
 } ) {
 	const wrapperNode = useRef();
 	const instanceId = useInstanceId( LinkControl );
@@ -263,13 +262,21 @@ function LinkControl( {
 									title: 'Loading link...',
 									url: 'loading...',
 								} );
-								const _result = await new Promise( ( resolve ) => {
-									setTimeout( () => {
-										resolve( {
-											title: 'Resolved Titlte',
-											url: '/some-revoled/slug',
-										} );
-									}, 5000 );
+								const newPage = await apiFetch( {
+									path: `/wp/v2/posts`,
+									data: {
+										title: inputValue,
+										content: '',
+										status: 'publish', // TODO: use publish?
+									},
+									method: 'POST',
+								} );
+								// TODO: handle error from API
+								onChange( {
+									id: newPage.id,
+									title: newPage.title.raw, // TODO: use raw or rendered?
+									url: newPage.link,
+									type: newPage.type,
 								} );
 								setIsResolvingLink( false );
 								setIsEditingLink( false );
