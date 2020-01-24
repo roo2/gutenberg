dcdf06b22cf9108b309a6ef7dbdb839a99db9fd1
Dave Smith <getdavemail@gmail.com>
2020-01-24 11:53:58 +0000

    Force repopulation of search results on create button failure.

--- packages/block-editor/src/components/link-control/index.js
+++ packages/block-editor/src/components/link-control/index.js
@@ -240,6 +240,11 @@ function LinkControl( {
 		return ( maybeURL ) ? handleDirectEntry( val, args ) : handleEntitySearch( val, args );
 	}, [ handleDirectEntry, fetchSearchSuggestions ] );
 
+	const forceUpdateInputValue = ( val ) => {
+		setInputValue( '' );
+		setInputValue( val );
+	};
+
 	// Render Components
 	const renderSearchResults = ( { suggestionsListProps, buildSuggestionItemProps, suggestions, selectedSuggestion, isLoading, isInitialSuggestions } ) => {
 		const resultsListClasses = classnames( 'block-editor-link-control__search-results', {
@@ -291,6 +296,7 @@ function LinkControl( {
 											setIsEditingLink( false );
 										} else {
 											setIsEditingLink( true );
+											forceUpdateInputValue( inputValue );
 										}
 									} }
 									key={ `${ suggestion.id }-${ suggestion.type }` }
